2007e33cb83fa06e0d6a87b8afb011da7d6455bf
Sudarikov_Iaroslav <yarik846@mail.ru>
2022-12-18 15:00:16 +0300

    feat: инициализация проекта

---
+++ src/main/java/ru/home/homeaccountingassistant/HomeAccountingAssistantApplication.java
@@ -0,0 +1,13 @@
+package ru.home.homeaccountingassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAccountingAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeAccountingAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/home/homeaccountingassistant/HomeAccountingAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.home.homeaccountingassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeAccountingAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
